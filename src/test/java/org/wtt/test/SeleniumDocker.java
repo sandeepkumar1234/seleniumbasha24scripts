c5c14346b1210861f66bf22208a5780a720ae408
sandeepkumar1234 <54533323+sandeepkumar1234@users.noreply.github.com>
2021-09-27 12:07:17 +0530

    Update SeleniumDocker.java

--- src/test/java/org/wtt/test/SeleniumDocker.java
+++ src/test/java/org/wtt/test/SeleniumDocker.java
@@ -7,15 +7,21 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Random;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.testng.Assert;
 import org.testng.ITestContext;
-import org.testng.TestRunner;
+
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
@@ -40,7 +46,7 @@ public class SeleniumDocker{
  		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
 		capabilities.setPlatform(Platform.LINUX);
 		driver = new RemoteWebDriver(new URL(PropertiesUtility.properties.getProperty("environment.url")), capabilities);
-  		System.out.println("driver loaded.................ra ");
+  		System.out.println("driver loaded................. ");
 		System.out.println(driver);
 	}
 	
@@ -49,7 +55,7 @@ public class SeleniumDocker{
 	public void afterTest() throws MalformedURLException {
 	}
 
-	@Test(priority=1)
+	@Test(priority=1,description = "Launching javatpoint application")
 	public void simpleTest() throws MalformedURLException {
 		// Launch website  
 		driver.navigate().to(PropertiesUtility.properties.getProperty("application.url")); 
@@ -59,7 +65,7 @@ public class SeleniumDocker{
 	}
 
 
-	@Test(priority=2)
+	@Test(priority=2,description = "verify checkbox click functionality")
 	public void testCheckBox() throws MalformedURLException {
 
 		WebElement element=driver.findElement(By.xpath("//*[@id=\"link\"]/div/ul/li[3]/a"));
@@ -68,9 +74,8 @@ public class SeleniumDocker{
 		String expectedText = element2.getText();
 		System.out.println("checkBox():"+expectedText);
 		Assert.assertEquals("Java Tutorial", expectedText);
-		driver.navigate().refresh();
 	}
-
+    
 @Test(priority=3,description = "verify enter the text in input feild")
 	
 public void SearchText() throws AWTException, InterruptedException {
@@ -135,7 +140,7 @@ public void AlertPopup() {
 
 public void LaunchApplication1()
 {
-	driver.get("http://demo.guru99.com/v4/");
+	driver.navigate().to("http://demo.guru99.com/v4/");
 	System.out.println("Launched application successfully");
 }
 
@@ -154,10 +159,8 @@ public void Login1() {
 @Test(priority=14,description = "verify Adding Customer")
 public void AddCustomer() {
 	driver.findElement(By.xpath("/html/body/div[3]/div/ul/li[2]/a")).click();
-	Random r = new Random();
-	int max = 1000;
-	String name1 = "SampleName"+r.nextInt(max);
-	driver.findElement(By.name("name")).sendKeys(name1);
+	String name = randomestring();
+	driver.findElement(By.name("name")).sendKeys(name);
 	driver.findElement(By.name("rad1")).sendKeys("male");
 	driver.findElement(By.name("dob")).sendKeys("10/23/1990");
 	driver.findElement(By.name("addr")).sendKeys("INDIA");
@@ -165,18 +168,30 @@ public void AddCustomer() {
 	driver.findElement(By.name("state")).sendKeys("Telangana");
 	driver.findElement(By.name("pinno")).sendKeys("518009");
 	driver.findElement(By.name("telephoneno")).sendKeys("9876543210");
-	driver.findElement(By.name("email")).sendKeys(name1+"@gmail.com");
+	driver.findElement(By.name("emailid")).sendKeys(name+"@gmail.com");
 	driver.findElement(By.name("password")).sendKeys("testing");
 	driver.findElement(By.name("sub")).click();
+}
+
+public String randomestring()
+{
+  String generatestring = RandomStringUtils.randomAlphabetic(6);
+  return(generatestring);
 }
 
 @Test(priority=15,description = "verify Logout")
 
 public void Logout() {
-	driver.findElement(By.xpath("@Test(priority=8,description = \"verify Adding Customer\")")).click();
+	driver.findElement(By.xpath("/html/body/div[3]/div/ul/li[15]/a")).click();
 	System.out.println("Logout Successfully");
 	
 }
 
-}  
+@Test(priority=16,description = "verify AlertPopup")
 
+public void AlertPopup1() throws InterruptedException {
+	Thread.sleep(5000);
+	driver.close();
+	
+}
+}
